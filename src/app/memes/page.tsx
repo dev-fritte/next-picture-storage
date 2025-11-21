f245129429545582c34ea1a37155881a87d5d7e4
Patrick Schmidt <p.schmidt@goconut.com>
2025-11-21 19:54:53 +0100

    changed blob name to a link, removed unused code

--- src/app/memes/page.tsx
+++ src/app/memes/page.tsx
@@ -1,34 +1,21 @@
-import {Meme} from 'src/app/memes/_types/meme-types'
 import {getBlobList} from 'src/app/memes/data'
-
-type MemeResponse = {
-    success: boolean,
-    data: {
-        memes: Meme[],
-    }
-}
 
 export default async function MemesOverview() {
 
     const blobs = await getBlobList();
-
-    // const memes = await fetch('https://api.imgflip.com/get_memes')
-    //     .then(res => res.json())
-    //     .then((resp: MemeResponse) => resp?.data?.memes)
 
     console.log('number of blobs: ', blobs.length)
 
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main
-                className="flex min-h-screen w-full flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+                className="flex min-h-screen w-full flex-col gap-4 items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <p>{'Memes Overview'}</p>
 
                 <div className={'flex flex-wrap gap-2'}>
-                    {/*{memes.map((meme: Meme) => (*/}
-                    {/*    <Image src={meme.url} alt={meme.name} key={meme.id} width={200} height={200}/>))}*/}
-
-                    {blobs.map(blob => <p key={blob.pathname}>{blob.pathname}</p>)}
+                    {blobs.map(blob => <a key={blob.pathname} href={blob.url}>
+                        {blob.pathname}
+                    </a>)}
                 </div>
             </main>
         </div>
