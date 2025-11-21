68009b123994948a6fcc0a7b312ec73e64ddd8d2
Patrick Schmidt <p.schmidt@goconut.com>
2025-11-21 19:59:39 +0100

    removed GET handler for /memes/upload

--- src/app/memes/upload/route.tsx
+++ src/app/memes/upload/route.tsx
@@ -1,19 +1,5 @@
-import {getMemes} from 'src/app/memes/data'
-import {list, put} from '@vercel/blob';
+import {put} from '@vercel/blob';
 import {NextRequest} from 'next/server'
-
-export async function GET(request: Request) {
-    console.log('executing POST route handler');
-    console.log('request', request)
-
-    const blobList = await list();
-
-    console.log('blobList', blobList);
-
-    const memes = await getMemes();
-
-    return Response.json(memes)
-}
 
 export async function POST(request: NextRequest) {
     console.log('executing POST route handler');
@@ -21,6 +7,7 @@ export async function POST(request: NextRequest) {
 
     const data = await request.formData();
     console.log('data', data);
+
     const file = data.get('file') as File;
 
     const blob = await put(file.name, file, {
