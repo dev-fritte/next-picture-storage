0942389c6dc66a26152d74f4eb59a1e3b46f239e
Patrick Schmidt <p.schmidt@goconut.com>
2025-11-26 21:15:34 +0100

    changed environment keys for supabase

--- src/supabase/serverClient.ts
+++ src/supabase/serverClient.ts
@@ -1,11 +1,16 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import {createServerClient} from '@supabase/ssr'
+import {cookies} from 'next/headers'
 
 export async function createClient() {
     const cookieStore = await cookies()
+
+    console.log('create supabase client with credentials')
+    console.log('public URL', process.env.SUPABASE_URL, process.env.SUPABASE_URL!)
+    console.log('key: ', process.env.SUPABASE_PUBLISHABLE_KEY, process.env.SUPABASE_PUBLISHABLE_KEY!)
+
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+        process.env.SUPABASE_URL!,
+        process.env.PUBLISHABLE_KEY!,
         {
             cookies: {
                 getAll() {
@@ -13,7 +18,7 @@ export async function createClient() {
                 },
                 setAll(cookiesToSet) {
                     try {
-                        cookiesToSet.forEach(({ name, value }) => cookieStore.set(name, value))
+                        cookiesToSet.forEach(({name, value}) => cookieStore.set(name, value))
                     } catch {
                         // The `setAll` method was called from a Server Component.
                         // This can be ignored if you have middleware refreshing
