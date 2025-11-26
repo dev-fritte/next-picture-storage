0d7ded28815e6ad48d14fe6ee9e460a838ed2e9c
Patrick Schmidt <p.schmidt@goconut.com>
2025-11-26 23:29:05 +0100

    removed supabase client logs

--- src/supabase/serverClient.ts
+++ src/supabase/serverClient.ts
@@ -4,10 +4,6 @@ import {cookies} from 'next/headers'
 export async function createClient() {
     const cookieStore = await cookies()
 
-    console.log('create supabase client with credentials')
-    console.log('public URL', process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_URL!)
-    console.log('key: ', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
-
     return createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
