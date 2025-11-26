8a6f69d858831f94a93933b6e83122aa8e9bb733
Patrick Schmidt <p.schmidt@goconut.com>
2025-11-26 20:31:13 +0100

    moved routes file to upload directory

--- src/app/memes/upload/route.tsx
+++ src/app/memes/upload/route.tsx
@@ -1,15 +1,20 @@
 import {put} from '@vercel/blob';
 import {NextRequest} from 'next/server'
+import {createClient} from 'src/supabase/serverClient'
+import {PICTURE_TABLE} from 'src/supabase/tablenames'
+import {MemeEntry} from 'src/app/memes/_types/meme-entry-types'
 
 export async function POST(request: NextRequest) {
-    console.log('executing POST route handler');
+    console.log('executing GET route handler for /memes');
     console.log('request', request)
 
     const data = await request.formData();
     console.log('data', data);
 
     const file = data.get('file') as File;
-    const tagArray = data.get('tags');
+    const tagArray = data.get('tags') as unknown as string[];
+    console.log('tags', tagArray);
+    tagArray.forEach((tag) => tag.toLowerCase())
 
     const blob = await put(file.name, file, {
         access: 'public',
@@ -17,6 +22,21 @@ export async function POST(request: NextRequest) {
     })
 
     console.log('blob', blob)
+
+    const supabase = await createClient();
+
+    console.log('client created', supabase)
+
+    const {data: entries} = await supabase.from(PICTURE_TABLE).select();
+
+    console.log('entries', entries);
+
+    const supabaseResponse = await supabase
+        .from(PICTURE_TABLE)
+        .insert({blob_url: blob.downloadUrl, tags: tagArray} as MemeEntry)
+        .select();
+
+    console.log('error', supabaseResponse)
 
     return Response.json(blob.url);
 }
